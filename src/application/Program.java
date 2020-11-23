cab6936c5ad4e3e5d6b2eaa80ea40a16e392d5a0
motasjw <lucasm.lopes087@gmail.com>
2020-11-23 10:59:18 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,7 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+			
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
@@ -47,6 +48,7 @@ public class Program {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
+			
 		}
 		
 
